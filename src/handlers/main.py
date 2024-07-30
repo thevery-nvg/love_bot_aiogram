7932eed90c29ef3a2e50b6a8e86cb4c7951a9c00
box7 <theverynvg@vk.com>
2024-07-31 01:52:15 +0400

    handlers rebuild, file validators created

--- src/handlers/main.py
+++ src/handlers/main.py
@@ -5,7 +5,7 @@ from src.database.db import add_user_if_not_exists, update_user
 from src.keyboards.questionary import *
 from src.database.models import Gender
 from src.states.ddd import *
-
+from src.utils.validators import *
 main_router = Router()
 
 
@@ -17,7 +17,7 @@ async def start(msg: types.Message, bot: Bot, db_pool) -> None:
         f'Hello, <a href="tg://user?id={msg.from_user.id}">{html.quote(msg.from_user.full_name)}</a>'
     ]
     await msg.answer("\n".join(m))
-    await add_user_if_not_exists(db_pool,msg.from_user.id, msg.from_user.full_name)
+    await add_user_if_not_exists(db_pool, msg.from_user.id, msg.from_user.full_name)
     await bot.send_message(msg.from_user.id, "keyboard", reply_markup=register_keyboard)
 
 
@@ -39,40 +39,37 @@ async def start_filling(call: types.CallbackQuery, state: FSMContext, bot: Bot) 
 
 
 @main_router.callback_query(QuestionAction.filter(F.question == Questions.start), Anketa.age)
-async def ask_age(call: types.CallbackQuery, state: FSMContext, bot: Bot) -> None:
+async def ask_age(call: types.CallbackQuery, bot: Bot) -> None:
     await bot.send_message(call.from_user.id, "Введите ваш возраст")
-    await state.set_state(Anketa.gender)
 
 
-@main_router.message(Anketa.gender)
-async def ask_gender(message: types.Message, state: FSMContext, bot: Bot) -> None:
-    # Проверить является ли введенное значение числом
-    age = message.text.strip()
-    await state.update_data(age=age)
-    await bot.send_message(message.from_user.id, "Выбирите ваш пол", reply_markup=gender_keyboard)
-    await state.set_state(Anketa.looking_for)
+@main_router.message(Anketa.age)
+async def receive_age_ask_gender(msg: types.Message, state: FSMContext, bot: Bot) -> None:
+    if validate_age(msg.text):
+        await state.update_data(age=int(msg.text))
+        await bot.send_message(msg.from_user.id, "Выберите ваш пол", reply_markup=gender_keyboard)
+        await state.set_state(Anketa.gender)
+    else:
+        await bot.send_message(msg.from_user.id, "Введите корректный возраст(число от 18 до 99)")
 
 
-@main_router.callback_query(QuestionAction.filter(F.question == Questions.gender_male),
-                            QuestionAction.filter(F.question == Questions.gender_female),
-                            Anketa.looking_for)
-async def ask_looking_for(call: types.CallbackQuery, state: FSMContext, bot: Bot) -> None:
+@main_router.callback_query(Anketa.gender)
+async def receive_gender_ask_looking_for(call: types.CallbackQuery,
+                                         state: FSMContext,
+                                         bot: Bot) -> None:
     if call.data == QuestionAction(question=Questions.gender_male).pack():
         await state.update_data(gender=Gender.male)
     elif call.data == QuestionAction(question=Questions.gender_female).pack():
         await state.update_data(gender=Gender.female)
-    await bot.send_message(call.from_user.id, "Кого хотите найти",
+    await bot.send_message(call.from_user.id, "Кого хотите найти?",
                            reply_markup=looking_for_keyboard)
-    await state.set_state(Anketa.location)
+    await state.set_state(Anketa.looking_for)
 
 
-@main_router.callback_query(
-    QuestionAction.filter(F.question == Questions.looking_for_male),
-    QuestionAction.filter(F.question == Questions.looking_for_female),
-    QuestionAction.filter(F.question == Questions.looking_for_anything),
-    Anketa.location
-)
-async def ask_location(call: types.CallbackQuery, state: FSMContext, bot: Bot) -> None:
+@main_router.callback_query(Anketa.looking_for)
+async def receive_looking_for_ask_location(call: types.CallbackQuery,
+                                           state: FSMContext,
+                                           bot: Bot) -> None:
     if call.data == QuestionAction(question=Questions.looking_for_male).pack():
         await state.update_data(looking_for=Gender.male)
     elif call.data == QuestionAction(question=Questions.looking_for_female).pack():
@@ -81,44 +78,58 @@ async def ask_location(call: types.CallbackQuery, state: FSMContext, bot: Bot) -
         await state.update_data(looking_for=Gender.anything)
     await bot.send_message(call.from_user.id, "Отправьте ваше местоположение или напишите город",
                            reply_markup=get_location_keyboard)
-    await state.set_state(Anketa.name)
+    await state.set_state(Anketa.location)
+
+
+@main_router.message(Anketa.location)
+async def receive_location_ask_name(message: types.Message,
+                                    state: FSMContext,
+                                    bot: Bot) -> None:
+    if message.location is not None:
+        latitude = message.location.latitude
+        longitude = message.location.longitude
+        await state.update_data(longitude=longitude, latitude=latitude)
+    else:
+        city = message.text.strip()
+        if validate_city(city):
+            await state.update_data(city=city)
+            await bot.send_message(message.from_user.id, "Ваше имя")
+            await state.set_state(Anketa.name)
+        else:
+            await bot.send_message(message.from_user.id, "Введите корректный город")
+            await state.update_data(city=city)
 
 
 @main_router.message(Anketa.name)
-async def ask_name(message: types.Message, state: FSMContext, bot: Bot) -> None:
-    # Проверить введены координаты или город
-    # Если город, то проверить корректность ввода
-    latitude = message.location.latitude
-    longitude = message.location.longitude
-    await state.update_data(longitude=longitude, latitude=latitude)
-    await bot.send_message(message.from_user.id, "Ваше имя")
-    await state.set_state(Anketa.description)
+async def receive_name_ask_description(message: types.Message,
+                                       state: FSMContext,
+                                       bot: Bot) -> None:
+    if validate_name(message.text):
+        await state.update_data(name=message.text.strip())
+        await bot.send_message(message.from_user.id, "О себе и кого хочешь найти,\n"
+                                                     "чем предлагаешь заняться.\n"
+                                                     "Это поможет лучше подобрать тебе кого-то")
+        await state.set_state(Anketa.description)
+    else:
+        await bot.send_message(message.from_user.id, "Введите корректное имя")
 
 
 @main_router.message(Anketa.description)
-async def ask_description(message: types.Message, state: FSMContext, bot: Bot) -> None:
-    # Имя должно содержать только буквы
-    await state.update_data(name=message.text.strip())
-    await bot.send_message(message.from_user.id, "Расскажи о себе и кого хочешь найти,\n"
-                                                 "чем предлагаешь заняться.\n"
-                                                 "Это поможет лучше подобрать тебе кого-то")
-    await state.set_state(Anketa.photo)
-
-
-@main_router.message(Anketa.photo)
-async def ask_photo(message: types.Message, state: FSMContext, bot: Bot) -> None:
+async def receive_desc_ask_photo(message: types.Message,
+                                 state: FSMContext,
+                                 bot: Bot) -> None:
     await state.update_data(description=message.text.strip())
     await bot.send_message(message.from_user.id, "Отправьте фото")
-    await state.set_state(Anketa.done)
-    await state.update_data(photo=[])
+    await state.set_state(Anketa.photo)
+    await state.update_data(photos=[])
 
 
-@main_router.message(Anketa.done, content_type="photo")
-async def ask_photo(message: types.Message, state: FSMContext, bot: Bot) -> None:
+@main_router.message(Anketa.photo, content_type="photo")
+async def receive_photo(message: types.Message, state: FSMContext, bot: Bot) -> None:
     data = await state.get_data()
-    photos = data.get("photo")
+    photos = data.get("photos")
     photos.append(message.photo[-1].file_id)
-    await state.update_data(photo=photos)
+    await state.update_data(photos=photos)
     if len(photos) == 1:
         msg = ("Ваша анкета заполнена, спасибо!\n"
                "Нажмите на кнопку для завершения\n"
@@ -127,32 +138,31 @@ async def ask_photo(message: types.Message, state: FSMContext, bot: Bot) -> None
         msg = "Фото добавлено!Отправьте еще или нажмите на кнопку для завершения"
     else:
         msg = "Ваша анкета заполнена, спасибо!\nНажмите на кнопку для завершения"
-
-    await bot.send_message(message.from_user.id, msg,
-                           reply_markup=done_filling_keyboard)
+    await bot.send_message(message.from_user.id, msg, reply_markup=done_filling_keyboard)
 
 
-@main_router.callback_query(QuestionAction.filter(F.question == Questions.done_filling),
-                            Anketa.done)
+@main_router.message(Anketa.photo)
+async def invalid_photo(message: types.Message, bot: Bot) -> None:
+    await bot.send_message(message.from_user.id, "Отправьте только фото")
+
+
+@main_router.callback_query(QuestionAction.filter(F.question == Questions.done_filling))
 async def done_filling(call: types.CallbackQuery, state: FSMContext, db_pool) -> None:
     # unfinished
     data = await state.get_data()
-    age: int = data.get("age")
-    gender: Gender = data.get("gender")
-    looking_for: Gender = data.get("looking_for")
-    name: str = data.get("name")
-    description: str = data.get("description")
-    photos: list[str] = data.get("photo")
-    longitude: str = data.get("longitude")
-    latitude: str = data.get("latitude")
-    await update_user(db_pool,
-                      user_id=call.from_user.id,
-                      age=age,
-                      gender=gender,
-                      looking_for=looking_for,
-                      name=name,
-                      description=description,
-                      photos=photos,
-                      latitude=latitude,
-                      longitude=longitude,
-                      )
+    profile = dict()
+    profile["age"]: int = data.get("age")
+    profile["gender"]: Gender = data.get("gender")
+    profile["looking_for"]: Gender = data.get("looking_for")
+    profile["name"]: str = data.get("name")
+    profile["description"]: str = data.get("description")
+    profile["photos"]: list[str] = data.get("photos")
+    longitude: str = data.get("longitude", None)
+    latitude: str = data.get("latitude", None)
+    if longitude is not None and latitude is not None:
+        profile["longitude"] = longitude
+        profile["latitude"] = latitude
+    else:
+        profile["city"] = data.get("city")
+    await update_user(db_pool, user_id=call.from_user.id, **profile)
+
---
+++ src/utils/validators.py
@@ -0,0 +1,10 @@
+def validate_age(age):
+    ...
+
+
+def validate_city(city):
+    ...
+
+
+def validate_name(name):
+    ...
